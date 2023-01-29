fa3881538a5e77755ce5f55a4547eaaecd257eb9
Parth Tyagi <parthtyagicode@gmail.com>
2023-01-29 14:33:50 +0530

    made some changes

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const firestore = require('firebase/firestore');
 const firebaseConfig = require('./firebaseConfig.js');
 const nodemailer = require('nodemailer');
 const crypto = require('crypto');
+const uuid = require('uuid');
 
 //----------------------------------------------------------------------------------------------
 
@@ -24,46 +25,70 @@ let transporter = nodemailer.createTransport({
     port: 587,
     secure: false,
     auth: {
-        user: 'parthtyagi414@gmail.com',
-        pass: 'osgcdxrfgsbfvzkj'
+        user: 'team.yuvaandtu@gmail.com',
+        pass: 'xlvytxeakduxtywf'
     },
     tls: {
         rejectUnauthorized: false
     }
 });
 
-//----------------------------------------------------------------------------------------------
+const getId = () => {
+    let str = '';
+    for (let i = 0; i < 5; i++) {
+        str = str + Math.floor(Math.random() * 9).toString();
+    }
+    return str;
+}
 
+//----------------------------------------------------------------------------------------------
 
 app.get('/', async (req, res) => {
 
     const response = await firestore.getDocs(collectionRef);
-    const d = response.docs.map((user) => {
-        return user.id;
-    })
+    // const d = response.docs.map((user) => {
+    //     return user.id;
+    // })
 
     res.render('index');
 });
 
 app.post('/', async (req, res) => {
     const data = req.body;
-    const response = await firestore.addDoc(collectionRef, data);
-    const randomString = crypto.randomBytes(3).toString('hex');
-    // console.log(randomString);
+    data.fname = data.fname.toUpperCase();
+    data.lname = data.lname.toUpperCase();
+    const randomString = getId();
+    data.userId = randomString;
+
+    try {
+        const response = await firestore.addDoc(collectionRef, data);
+    }
+    catch (e) {
+        console.log('cannot add user to firestore!');
+    }
 
     let mailOptions = {
-        from: 'parthtyagi414@gmail.com',
+        from: 'team.yuvaandtu@gmail.com',
         to: req.body.email,
         subject: 'Yuvaan2023 Pass',
-        html: `<h1>${randomString}</h1>`,
+        html: `<h1>${'YUVA2K23 / ' + randomString}</h1>`,
         text: 'yuvaan2023',
     };
 
-    const info = await transporter.sendMail(mailOptions);
-    // console.log(info.response);
+    try {
+        const info = await transporter.sendMail(mailOptions);
+    }
+    catch (e) {
+        console.log('cannot send mail');
+        console.log(e);
+    }
 
     res.redirect('/');
 })
+
+
+//---------------------------------------------------------------------------------------------
+//using mongodb...
 
 
 
